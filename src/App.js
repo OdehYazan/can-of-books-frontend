5af90969a4673c31306a110ff776d152a85c6d96
OdehYazan <eng.yazanodeh@gmail.com>
2021-07-12 17:59:46 +0300

    lab12

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
                 {isAuthenticated == true?  <BestBooks />: <Login />}
                    {/* {/* {isAuthenticated && */}
-                   <BestBooks />
+                  
                 
                 { !isAuthenticated && <Login />} 
               </Route >
